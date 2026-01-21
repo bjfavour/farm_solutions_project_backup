b154b6f6c051d3a8df8432a948ecb49a89b202ef
bjfavour <bj.favour1@gmail.com>
2026-01-21 17:03:15 +0100

    host

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'change-me-for-prod')
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
